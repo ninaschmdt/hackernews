f9a8d125f75e0cdfcdd8f5f4dcdc5c2c276e6cab
anton litvak <anton.k.litvak@gmail.com>
2022-04-21 18:47:48 +0200

    got the button and search bar working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-
+const ArticleElement = ({ data }) => {
+  //console.log(data);
+  return (
+    <div id="titleContainer" type="text">
+      {data.title}
+    </div>
+  );
+};
 
 function App() {
-  const [articles, setArticles] = useState ([])
-  const testURL = fetch("http://hn.algolia.com/api/v1/search?query=react")
-  .then((response) => {
-      return response.json();
-  }).then((data) => {
-    setArticles (data.hits) 
-      });
+  const [userInput, setUserInput] = useState("");
+  const [articles, setArticles] = useState([]);
 
-      // line 9 to 14 is the fetch
-    
-     
- 
-  
+  useEffect(() => {
+    fetch("http://hn.algolia.com/api/v1/search?query=react")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.hits);
+      });
+  }, []);
+
+  const search = () => {
+    console.log(userInput);
+  };
+
+  // line 9 to 14 is the fetch
+
+  //useEffect
+
   return (
     <div className="App">
-      
+      <input
+        id="ugly"
+        onChange={(event) => {
+          console.log(event.target.value);
+          setUserInput(event.target.value);
+        }}
+      />
+      <button onClick={search}>ugly button</button>
+      {articles.map((article) => {
+        return <ArticleElement key={article.title} data={article} />;
+      })}
     </div>
   );
 }
