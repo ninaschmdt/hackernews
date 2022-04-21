98498d3b24f89d408bc13ad675786eeb16bf62bf
Isaiah-Umuks <umukoroisaiah2@gmail.com>
2022-04-21 17:05:09 +0200

    add article stage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 function App() {
+  const [articles, setArticles] = useState ([])
   const testURL = fetch("http://hn.algolia.com/api/v1/search?query=react")
   .then((response) => {
       return response.json();
   }).then((data) => {
-    console.log(data);
-  });
+    setArticles (data.hits) 
+      });
+
+      // line 9 to 14 is the fetch
+    
+     
  
   
   return (
