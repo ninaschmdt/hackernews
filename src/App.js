c403e64d53f090eb6cc0cc2eb3f404184aa6e506
anton litvak <anton.k.litvak@gmail.com>
2022-04-22 15:06:23 +0200

    added spinner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { SpinnerCircular } from "spinners-react";
 
 const ArticleElement = ({ data }) => {
-  //console.log(data);
+  console.log(data);
   return (
     <div id="titleContainer" type="text">
-      {data.title}
+      <a href={data.url}> {data.title} </a>
     </div>
   );
 };
@@ -25,7 +26,14 @@ function App() {
   }, []);
 
   const search = () => {
-    console.log(userInput);
+    //console.log(userInput);
+    fetch("http://hn.algolia.com/api/v1/search?query=" + userInput)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.hits);
+      });
   };
 
   // line 9 to 14 is the fetch
@@ -37,14 +45,15 @@ function App() {
       <input
         id="ugly"
         onChange={(event) => {
-          console.log(event.target.value);
+          //console.log(event.target.value);
           setUserInput(event.target.value);
         }}
       />
       <button onClick={search}>ugly button</button>
       {articles.map((article) => {
-        return <ArticleElement key={article.title} data={article} />;
+        return <ArticleElement key={article.objectID} data={article} />;
       })}
+      <div>{articles.length !== 0 ? "" : <SpinnerCircular />}</div>
     </div>
   );
 }
